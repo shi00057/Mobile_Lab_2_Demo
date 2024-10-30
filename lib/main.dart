84a897dac07319a2a26c5460e71a74daaf9e7e00
Guokai Shi <shi00057@algonquinlive.com>
2024-10-30 19:16:15 -0400

    - Good Demo

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: items.isEmpty
                   ? Center(
                 child: Text(
-                  "There are no items in the list.",gi
+                  "There are no items in the list.",
                   style: TextStyle(fontSize: 16),
                 ),
               )
